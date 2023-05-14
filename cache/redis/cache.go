e4e59ed50bc024c7b49e24e7746e593072b7c0a1
Muathe Jamil <Muathe.Jamil@foothillsolutions.com>
2023-05-14 21:35:23 +0300

    enhance docs

--- cache/redis/cache.go
+++ cache/redis/cache.go
@@ -9,6 +9,8 @@ import (
 	"todolist-api/contracts"
 )
 
+const Base int = 10
+
 // Cache cache struct.
 type Cache struct {
 	host    string
@@ -36,27 +38,38 @@ func (cache *Cache) getClient() *redis.Client {
 
 // Set sets the todo to the redis cache.
 func (cache *Cache) Set(key uint64, value contracts.TodoDTO) error {
+	// get cache client.
 	client := cache.getClient()
+	// marshaling the value.
 	json, err := json2.Marshal(value)
+	// handle error if exist when marshaling.
 	if err != nil {
 		return errors.New("unable to set todo to cache")
 	}
-	// set value to key, and it will expire in the amount of seconds
-	formatUint := strconv.FormatUint(key, 10)
+	// convert key to string with base.
+	formatUint := strconv.FormatUint(key, Base)
+	// set value to key, and it will expire in the amount of minutes.
 	client.Set(formatUint, json, cache.expires*time.Minute)
 	return nil
 }
 
 // Get gets the todo from the redis cache.
 func (cache *Cache) Get(key uint64) (contracts.TodoDTO, error) {
+	// get cache client.
 	client := cache.getClient()
+	// converts key to string.
 	formatUint := strconv.FormatUint(key, 10)
+	// get the key value from the cache.
 	val, err := client.Get(formatUint).Result()
+	// handle error if exists.
 	if err != nil {
 		return contracts.TodoDTO{}, errors.New("unable to find todo in cache")
 	}
+	// init new todoDto.
 	todo := contracts.TodoDTO{}
+	// unmarshal value.
 	err = json2.Unmarshal([]byte(val), &todo)
+	// handle error in un-marshaling if exists.
 	if err != nil {
 		return contracts.TodoDTO{}, errors.New("unable to find todo in cache")
 	}
